6dbbd5db02afe44d67ee378214c18e6af00809e6
Daniil <danilayukin@gmail.com>
2024-06-18 20:46:09 +0300

    widget update

--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,31 @@
-from src.masks import get_mask_account
-from src.masks import get_mask_card_number
 from typing import Optional
+
+import masks
 
 
 def mask_account_card(cards_number: str) -> Optional[str]:
-    """ Функция которая маскирует номер карты и счета """
+    """Функция которая маскирует номер карты и счета"""
+
     if "Счет" in cards_number:
-        mask_account = get_mask_account(cards_number[:])
-        return mask_account
+        mask_account = masks.get_mask_account(cards_number[-20:])
+        return f"Счет {mask_account}"
     else:
-        card = get_mask_card_number(cards_number[-16:])
+        card = masks.get_mask_card_number(cards_number[-16:])
         mask_card = cards_number.replace(cards_number[-16:], card)
         return mask_card
 
 
-if __name__ == '__main__':
-    print(mask_account_card('''Maestro 1596837868705199
-Счет 64686473678894779589
-MasterCard 7158300734726758
-Счет 35383033474447895560
-Visa Classic 6831982476737658
-Visa Platinum 8990922113665229
-Visa Gold 5999414228426353
-Счет 73654108430135874305'''))
+def get_data(date: str) -> str:
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Счет 35383033474447895560"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(get_data("2018-07-11T02:26:18.671407"))
